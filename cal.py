98e7fd71a0e14f45756ae2adbf8131f3a794bbcb
Eryck Ferreira <132472671+eryckferreira@users.noreply.github.com>
2025-11-25 19:01:43 -0300

    Update cal.py

--- cal.py
+++ cal.py
@@ -2,10 +2,8 @@
 peso = float(input("Qual o peso? "))
 preco_por_tonelada = float(input("Qual o preço por tonelada? "))
 
-# -----------
-
 # definição da função estava incorreta
-def cliente_tipo():   # ALTERAD
+def cliente_tipo(): 
     tipo_cliente = int(input("Qual tipo de cliente? [1]Novo Cliente [2]Cliente fidelizado [3]Cliente Premium: "))  
     if tipo_cliente == 1:   
         print("Novo Cliente")
@@ -39,4 +37,5 @@ desconto_aplicado = desconto(tipo_cliente)
 valor_final = valor_total * (1 - desconto_aplicado)   
 
 # Exibe o resultado formatado com duas casas decimais
+
 print(f"Valor final: {valor_final:.2f}")   
